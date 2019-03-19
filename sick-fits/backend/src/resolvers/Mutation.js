5b2e9879a2237e8b0395d5398d28f08682be26ed
James Bowles <james.bowles@fluxfederation.com>
2019-03-20 11:37:19 +1300

    Item query and mutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,10 +1,14 @@
 const mutations = {
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || [];
-    const newDog = { name: args.name };
+  async createItem(parent, args, ctx, info) {
+    //TODO - check if logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: { ...args }
+      },
+      info
+    );
 
-    global.dogs.push(newDog);
-    return newDog;
+    return item;
   }
 };
 
