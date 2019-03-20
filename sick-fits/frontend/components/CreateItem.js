45d56b28fcec262ff69afa351abcf89fb7420d0e
James Bowles <james.bowles@fluxfederation.com>
2019-03-21 11:58:50 +1300

    Add image uploads to item create

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -25,7 +25,7 @@ const CREATE_ITEM_MUTATION = gql`
   }
 `;
 
-export default class CreateItem extends Component {
+class CreateItem extends Component {
   state = {
     title: "",
     description: "",
@@ -37,6 +37,28 @@ export default class CreateItem extends Component {
     const { name, type, value } = e.target;
     const val = type == "number" ? parseFloat(value) : value;
     this.setState({ [name]: val });
+  };
+
+  uploadFile = async e => {
+    const files = e.target.files;
+    const data = new FormData();
+    data.append("file", files[0]);
+    data.append("upload_preset", "sickfits");
+
+    const res = await fetch(
+      "https://api.cloudinary.com/v1_1/dkkzbufup/image/upload",
+      {
+        method: "POST",
+        body: data
+      }
+    );
+
+    const file = await res.json();
+    console.log(file);
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    });
   };
 
   render() {
@@ -56,6 +78,24 @@ export default class CreateItem extends Component {
           >
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload and image"
+                  required
+                  onChange={this.uploadFile}
+                />
+                {this.state.image && (
+                  <img
+                    width="200"
+                    src={this.state.image}
+                    alt="upload preview"
+                  />
+                )}
+              </label>
               <label htmlFor="title">
                 Title
                 <input
@@ -99,4 +139,5 @@ export default class CreateItem extends Component {
     );
   }
 }
+export default CreateItem;
 export { CREATE_ITEM_MUTATION };
